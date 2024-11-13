4a6e94b288d595ebfa463bfa6be0e03f06efe770
DA MAC M1 158 <damacm1158@DAs-MacBook-Air.local>
2024-11-13 16:12:21 +0200

    chore: Initialize chat-backend structure with configuration, models, and migrations

---
+++ chat-backend/database/migrations/20241113135906-create-user.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const { defaultValueSchemable } = require('sequelize/lib/utils');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      avatar: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ chat-backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const config = require('./config/app')
+
+app.get('/home' ,(req, res ) =>{
+    return res.send('<h1>Home Screen</h1>')
+})
+
+app.get('/login', (req, res) =>{
+    return res.send('login Screen')
+})
+
+const port = config.appPort
+
+app.listen(port, ()=> {
+    console.log("Sever listening on port" + " " + port)
+})
+console.log("Thamsanqa Shabangu")
